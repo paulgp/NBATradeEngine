3b0d15fb6f6ca0ea4d2d21e7feed3dec43a3c3fe
jacobwwallace <jacob.wallace@yale.edu>
2017-10-23 16:12:04 -0400

    squash! Add import.R script

--- import.R
+++ import.R
@@ -1,1 +1,4 @@
-BBMProjections <- read.csv(file="Data/BBM_Projections.csv", header=TRUE, sep=",")
+# This file imports the Basketball Monster Projections and saves dataframe
+
+PlayerProjs <- read.csv(file="Data/BBM_Projections.csv", header=TRUE, sep=",")
+save(PlayerProjs, file="Output/PlayerProjs.Rdata")
