592c066765dc5a13fb8d17253188a11990e5e487
jacobwwallace <jacob.wallace@yale.edu>
2017-10-24 18:17:25 -0400

    Added munging to import.R

--- import.R
+++ import.R
@@ -1,4 +1,13 @@
-# This file imports the Basketball Monster Projections and saves dataframe
+# This file imports the Basketball Monster Projections, munges and saves dataframe
 
 PlayerProjs <- read.csv(file="Data/BBM_Projections.csv", header=TRUE, sep=",")
+
+## Munge data  ----------------------------------------
+                                                       
+## Field goals made per game                           
+PlayerProjs$fgm.g = PlayerProjs$fga.g * PlayerProjs$fg.
+                                                       
+## Free throws made per game                           
+PlayerProjs$ftm.g = PlayerProjs$fta.g * PlayerProjs$ft.
+
 save(PlayerProjs, file="Output/PlayerProjs.Rdata")
