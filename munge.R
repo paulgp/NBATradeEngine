39991871625befd1d5810da8c3498708fd89a53f
jacobwwallace <jacob.wallace@yale.edu>
2017-10-24 12:53:22 -0400

    Added fcns to munge.R

--- munge.R
+++ munge.R
@@ -18,12 +18,35 @@ PlayerProjs$ftm.g = PlayerProjs$fta.g * PlayerProjs$ft.
 
 ## Construct team-level projections ------------------------------
 
-## Calculate means across rostered players
-TeamProjs <- PlayerProjs %>%
-    group_by(Owner) %>%
-    select(Owner, p.g, X3.g, r.g, a.g, s.g, b.g, fgm.g, fga.g, ftm.g, fta.g, to.g) %>%
-    summarize_all(.funs = c(mean="mean"))
+teamprojection_fun <- function(playerProj) {
 
-## Construct new fields for fg.g and ft.g
-TeamProjs <- TeamProjs %>%
-    mutate(fg.g = fgm.g_mean/ fga.g_mean, ft.g = ftm.g_mean / fta.g_mean)
+    ## Collapse data from player to team level
+    teamProj <- playerProj %>%
+        group_by(Owner) %>%
+        filter(Owner!="") %>%
+        select(Owner, points = p.g, threes = X3.g, rebounds = r.g, assists = a.g, steals = s.g, blocks = b.g, fgmade = fgm.g, fgattempts = fga.g, ftmade = ftm.g, ftattempts = fta.g, turnovers  = to.g) %>%
+        summarize_all(.funs = c(mean))
+
+    ## Construct new fields for fg.g and ft.g
+    teamProj <- teamProj %>%
+        mutate(fgperc = fgmade/ fgattempts, ftperc = ftmade / ftattempts) %>%
+        select(Owner, points, threes, rebounds, assists, steals, blocks, fgperc, ftperc, turnovers)
+    return(teamProj)
+
+}
+
+## Calculate total columns won for each team
+
+teamwins_fun <- function(teamProj) {
+
+    RankedProj <- select(teamProj,-Owner) %>%
+        mutate_all(funs(dense_rank(desc(-.))-1)) %>%
+        mutate(wins = rowSums(., na.rm = TRUE))
+    
+    teamWins <- cbind(teamProj$Owner,RankedProj$wins)
+
+    return(teamWins)
+}
+
+teamProj <- teamprojection_fun(PlayerProjs)
+teamRank <- teamrank_fun(teamProj)
